968a1701a64f6e873e7cd0476b01a1fad08854d1
josemacielfj <josemacielfj@gmail.com>
2023-08-14 00:12:55 -0300

    Everything working

--- client/index.js
+++ client/index.js
@@ -2,16 +2,25 @@ const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
+
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // DONE: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  
+  let data;
+  niceList.forEach(async (name, i) => {
+    const proof = merkleTree.getProof(i);
+    dataToServer = {
+      'name': name,
+      'proof': proof
+    }   
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, dataToServer);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    console.log({ gift });
   });
-
-  console.log({ gift });
+  
 }
 
 main();
--- utils/example.js
+++ utils/example.js
@@ -7,6 +7,7 @@ const merkleTree = new MerkleTree(niceList);
 
 // get the root
 const root = merkleTree.getRoot();
+console.log(root);
 
 // find the proof that norman block is in the list 
 const name = 'Norman Block';
@@ -17,3 +18,10 @@ const proof = merkleTree.getProof(index);
 console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
+const name2 = 'Nelson Machado';
+const index2 = niceList.findIndex(n => n === name2);
+const proof2 = merkleTree.getProof(index2);
+
+console.log( verifyProof(proof2, name2, root) ); // false, Nelson Machado is NOT in the list!
+
+
